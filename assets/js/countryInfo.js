fb9159c1ab5c7de32d2f9a2db4d241eb99efc033
KaioDev08 <kaioaraujo6190@outlook.com>
2022-08-19 19:04:15 -0300

    Logica de busca de ultimo nome nativo e estrutura página de país

--- assets/js/countryInfo.js
+++ assets/js/countryInfo.js
@@ -8,12 +8,44 @@ function exibirPais() {
     const dadosPais = JSON.parse(sessionStorage.getItem('PaisSelecionado'));
     const main = document.querySelector('main');
 
-    const divImg = document.createElement('section')
+    const sectionImg = document.createElement('section')
+    const sectionInfo = document.createElement('section');
 
-    divImg.innerHTML = `
+    sectionImg.innerHTML = `
         <section>
             <img src=${dadosPais.flags.svg}>
         </section>
-    `
-    main.appendChild(divImg);
+    `;
+
+    sectionInfo.innerHTML = `
+        <h2>${dadosPais.name.common}</h2>
+        <div>
+            <ul>
+                <li><strong>Native Name:${encontrarUltimoNomeNativoComum(dadosPais)
+                }    
+                
+                </li>
+                <li><strong>Population:</strong>${dadosPais.population}</li>
+                <li><strong>Region:</strong>${dadosPais.region}</li>
+                <li><strong>Sub Region:</strong>${dadosPais.subregion}</li>
+                <li><strong>Capital:</strong>${dadosPais.capital[0]}</li>
+            </ul>
+            <ul>
+                <li><strong>Top Level Domain:</strong>${dadosPais.tld[0]}</li>
+                <li><strong>Currencies:</strong></li>
+                <li><strong>Languages:</strong></li>
+            </ul>
+        </div>
+        
+    `;
+    
+    main.appendChild(sectionImg);
+    main.appendChild(sectionInfo);
+}
+
+function encontrarUltimoNomeNativoComum(dadosPais){
+    const JSONNomes = JSON.stringify(dadosPais.name.nativeName);
+    const Ultimonome = JSONNomes.substring(JSONNomes.lastIndexOf(':') + 2 ,JSONNomes.length-3);
+
+    console.log(Ultimonome);
 }
